f51e9a903848472db16bebeeccb3e4b46c0b91bc
hongyehenry <mengwang@Mengs-MacBook-Pro.local>
2016-05-11 10:05:40 -0400

    inits

---
+++ client/javascript/controllers/HomeController.js
@@ -0,0 +1,12 @@
+app.controller('HomeController', ['$scope', 'activities', function ($scope, activities) {
+//      activities.success(function(data){
+//         data = data.activities;
+//        $scope.activities = data;
+//      });
+    activities.success(function (data) {
+        console.log(data);
+        console.log($scope.activities);
+        $scope.activities = data.activities;
+        console.log($scope.activities);
+    })
+}]);
---
+++ client/javascript/services/activities.js
@@ -0,0 +1,44 @@
+app.factory('activities', ['$http', function ($http) {
+    var lat, lng;
+//    return new Promise(function (resolve, reject) {
+        
+//    });
+    var url = '/api/activities/findByCoordinates?lat=40.6249614&lng=-74.0017677';
+    return $http.get(url).success(function(data) { 
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                lat = position.coords.latitude;
+                lng = position.coords.longitude;
+                console.log("Latitude : " + lat + " Longitude : " + lng);
+                url = '/api/activities/findByCoordinates?lat=' + lat + '&lng=' + lng
+                console.log(url);
+//                return $http.get(url).success(function(data) {
+//                    resolve(data);
+//                })
+            }); 
+        return data;
+    }).error(function(err) { 
+        return err; 
+    }); 
+}]);
+
+
+
+//app.factory('activities', ['$http', function ($http) {
+//    var lat, lng;
+//    return new Promise(function (resolve, reject) {
+//        navigator.geolocation.getCurrentPosition(
+//            function (position) {
+//                lat = position.coords.latitude;
+//                lng = position.coords.longitude;
+//                console.log(position);
+//                var url = '/api/activities/findByCoordinates?lat=' + lat + '&lng=' + lng
+//                console.log(url);
+//                $http.get(url).success(function(data) {
+//                    resolve(data);
+//                })
+//            })
+//    })
+//
+//
+//            }]);
